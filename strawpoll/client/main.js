3b2edcfb9b972ac6b3100189efdb6896388c870b
Shiisoo <defer1616@gmail.com>
2020-01-16 17:33:59 +0100

    programme

---
+++ strawpoll/client/main.js
@@ -0,0 +1,22 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+import '../collections/polls.js';
+import './components/poll.html'
+import './components/poll-form.html'
+
+Template.body.helpers({
+	polls: function() {
+		return Polls.find();
+	}
+});
+
+UI.registerHelper('indexedArray', function(context, options) {
+  if (context) {
+    return context.map(function(item, index) {
+      item._index = index;
+      return item;
+    });
+  }
+});
